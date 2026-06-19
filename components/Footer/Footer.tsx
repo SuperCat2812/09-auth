0377a9d77a87c2c85616ecaa0c4acfdb7462f59b
SuperCat2812 <64689542+SuperCat2812@users.noreply.github.com>
2026-06-19 18:41:19 +0200

    fix: update developer name and contact email in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,10 +5,10 @@ export default function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: your name</p>
+          <p>Developer: Oleksandr Karpenko</p>
           <p>
             Contact us:
-            <a href="mailto:student@notehub.app">student@notehub.app</a>
+            <a href="mailto:sashka281205@gmail.com">sashka281205@gmail.com</a>
           </p>
         </div>
       </div>
